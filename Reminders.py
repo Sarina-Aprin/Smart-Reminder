5a9a3f4a2336688f704bbfe9cb04cb5255383b27
Sarina <Sarinaa594@gmail.com>
2025-12-05 03:25:28 +0330

    Reminder base class deleted

--- Reminders.py
+++ Reminders.py
@@ -1,14 +1,6 @@
 from dataclasses import dataclass
 
 
-@dataclass
-class Reminder:
-    title: str
-    time: str
-    reminder_id: int
-
-    def remind(self):
-        raise NotImplementedError("Subclasses must implement remind()")
 
 @dataclass
 class SimpleReminder(Reminder):
