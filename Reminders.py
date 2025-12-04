0995a332ac85c70ce163f9dc40f8257b9732586c
Sarina <Sarinaa594@gmail.com>
2025-12-05 03:10:49 +0330

    added MeetingReminder with the participants list

--- Reminders.py
+++ Reminders.py
@@ -14,3 +14,9 @@ class Reminder:
 class SimpleReminder(Reminder):
     def remind(self):
         return f"It is time: {self.title}"
+
+@dataclass
+class MeetingReminder(Reminder):
+    participants: list[str]
+    def remind(self):
+        return f"Meeting Reminder: {', '.join(self.participants)} meeting with: {self.title}"
