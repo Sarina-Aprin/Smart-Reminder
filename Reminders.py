e5356ff6460933486a176dbedd5b178ba2638614
Sarina <Sarinaa594@gmail.com>
2025-12-05 03:12:16 +0330

    added DailyRoutineReminder with daily repeat flag

--- Reminders.py
+++ Reminders.py
@@ -20,3 +20,10 @@ class MeetingReminder(Reminder):
     participants: list[str]
     def remind(self):
         return f"Meeting Reminder: {', '.join(self.participants)} meeting with: {self.title}"
+
+@dataclass
+class DailyRoutineReminder(Reminder):
+    daily_repeat: bool
+    def remind(self):
+        status = "Active" if self.daily_repeat else "Off"
+        return f"Daily Routine: ({status} Daily repeat) {self.title}"
