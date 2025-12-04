988b1cc8aa48d23cbcb24186bec83fa66de49fd3
Sarina <Sarinaa594@gmail.com>
2025-12-05 03:26:07 +0330

    Reminder base class added

--- Reminders.py
+++ Reminders.py
@@ -1,6 +1,13 @@
 from dataclasses import dataclass
 
+@dataclass
+class Reminder:
+    title: str
+    time: str
+    reminder_id: int
 
+    def remind(self):
+        raise NotImplementedError("Subclasses must implement remind()")
 
 @dataclass
 class SimpleReminder(Reminder):
