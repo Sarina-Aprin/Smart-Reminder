efbd9333cb22fe9d207454285e9499ee41c2088f
Sarina <Sarinaa594@gmail.com>
2025-12-05 03:08:18 +0330

    added SimpleReminder subclass

--- Reminders.py
+++ Reminders.py
@@ -14,16 +14,3 @@ class Reminder:
 class SimpleReminder(Reminder):
     def remind(self):
         return f"It is time: {self.title}"
-
-@dataclass
-class MeetingReminder(Reminder):
-    participants: list
-    def remind(self):
-        return f"Meeting Reminder: {', '.join(self.participants)} meeting with: {self.title}"
-
-@dataclass
-class DailyRoutineReminder(Reminder):
-    daily_repeat: bool
-    def remind(self):
-        repeat_status = "Active" if self.daily_repeat else "Off"
-        return f"Daily Routine: ({repeat_status} Daily repeat) {self.title}"
